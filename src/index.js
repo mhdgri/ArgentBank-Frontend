fc60b15496f9afb2ea027d7afcfccfd8057d087c
mhdgri <mohamed.guerroui.mg@gmail.com>
2024-11-24 23:13:10 +0100

    Refactor index.js to use PersistGate for rendering the application after the store is ready

--- src/index.js
+++ src/index.js
@@ -2,34 +2,47 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import AppLayout from "./AppLayout";
 import SignInPage from "./pages/SignIn";
 import MainPage from "./pages/Main";
 import UserPage from "./pages/User";
 import GuardedRoute from "./components/GuardedRoute";
-import { store } from "./store/index";
-import 'font-awesome/css/font-awesome.min.css';
+import { store, persistor } from "./store/index";
+import "font-awesome/css/font-awesome.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<AppLayout />}>
+                <Route index element={<MainPage />} />
+                <Route path="sign-in" element={<SignInPage />} />
+
+                <Route
+                    path="user"
+                    element={
+                        <GuardedRoute>
+                            <UserPage />
+                        </GuardedRoute>
+                    }
+                />
+            </Route>
+        </Routes>
+    );
+};
+
 root.render(
     <BrowserRouter>
         <Provider store={store}>
-            <Routes>
-                <Route path="/" element={<AppLayout />}>
-                    <Route index element={<MainPage />} />
-                    <Route path="sign-in" element={<SignInPage />} />
-
-                    <Route
-                        path="user"
-                        element={
-                            <GuardedRoute>
-                                <UserPage />
-                            </GuardedRoute>
-                        }
-                    />
-                </Route>
-            </Routes>
+            {/* 
+                Utilise le composant PersistGate pour attendre que le store soit prêt avant de faire le rendu l'application.
+            */}
+            <PersistGate persistor={persistor}>
+                <AppRoutes />
+            </PersistGate>
         </Provider>
     </BrowserRouter>
 );
