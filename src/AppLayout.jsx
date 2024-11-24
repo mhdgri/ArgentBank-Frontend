57c26525570263b46f85420d3e5f82ca1272e919
mhdgri <mohamed.guerroui.mg@gmail.com>
2024-11-24 23:30:20 +0100

    remove body from app layout and add flexbox to root div

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -1,17 +1,19 @@
 import React from "react";
+import { Outlet } from "react-router";
+
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import { Outlet } from "react-router";
+
 import "./assets/css/main.css";
 
 const App = () => {
+
     return (
-        
-        <body>
+        <>
             <NavBar />
             <Outlet />
             <Footer />
-        </body>
+        </>
     );
 };
 
