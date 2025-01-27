2e960d893841628a254f0cdb1f3ecc5f3f68b5f3
Mohamed Guerroui <mohamed.guerroui.mg@gmail.com>
2025-01-27 01:13:48 +0100

    Modification du code HTML du formulaire login en composant react

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,23 +1,47 @@
+import { useState } from "react";
+import InputField from "../components/InputField";
+import RememberMeCheckBox from "../components/RememberMeCheckBox";
 
 function Login() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [rememberMe, setRememberMe] = useState(false)
+    
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    console.log('Username:', username);
+    console.log('Password:', password);
+    console.log('RememberMe:', rememberMe);
+  }
+
+
     return (
       <main className="main bg-dark">
         <section className="sign-in-content">
           <i className="fa fa-user-circle sign-in-icon"></i>
           <h1>Sign In</h1>
-          <form>
-            <div className="input-wrapper">
-              <label htmlFor="username">Username</label>
-              <input type="text" id="username" />
-            </div>
-            <div className="input-wrapper">
-              <label htmlFor="password">Password</label>
-              <input type="password" id="password" />
-            </div>
-            <div className="input-remember">
-              <input type="checkbox" id="remember-me" />
-              <label htmlFor="remember-me">Remember me</label>
-            </div>
+          <form onSubmit={handleSubmit}>
+            <InputField 
+              label="username"
+              id="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <InputField 
+              label="password"
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <RememberMeCheckBox
+              type="checkbox"
+              id="remember-me"
+              checked={rememberMe}
+              onChange={() => setRememberMe(!rememberMe)}
+            />
             <button type="submit" className="sign-in-button">
               Sign In
             </button>
