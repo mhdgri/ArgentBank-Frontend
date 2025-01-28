6129b44577f3a106d47450014585f9bd77f5051a
Mohamed Guerroui <mohamed.guerroui.mg@gmail.com>
2025-01-28 01:43:55 +0100

    Creation du composant button pour les différentes transaction

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,12 @@
+
+
+function Button() {
+  return (
+    <div className="account-content-wrapper cta">
+        <button className="transaction-button">View transactions</button>
+    </div>
+  )
+}
+
+export default Button
+
