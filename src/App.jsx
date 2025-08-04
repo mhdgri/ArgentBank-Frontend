366e1b3add9b1db86f98bf12b38758eb49c3a289
Mohamed Guerroui <mohamed.guerroui.mg@gmail.com>
2025-08-04 03:56:28 +0200

    Ajout des composants Home et Login dans les routes de App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route></Route>
-        <Route></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="Login" element={<Login />} />
         <Route></Route>
       </Routes>
       <Footer />
