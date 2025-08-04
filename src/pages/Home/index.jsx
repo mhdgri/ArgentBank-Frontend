2495d4d70a0987c69b92df36967944c3adfc4e33
Mohamed Guerroui <mohamed.guerroui.mg@gmail.com>
2025-08-04 03:46:33 +0200

    Création de la page Login

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import Features from "../../components/Features"
+import Hero from "../../components/Hero"
+
+function Home() {
+  return (
+    <div>
+        Home
+    </div>
+  )
+}
+
+export default Home
