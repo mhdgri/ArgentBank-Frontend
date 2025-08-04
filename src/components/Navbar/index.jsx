a8f50507d5a73e6b52978f753a0dd0dbb9a97390
Mohamed Guerroui <mohamed.guerroui.mg@gmail.com>
2025-08-04 04:09:07 +0200

    Ajout de Link pour la navigation dans la Navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,24 +1,26 @@
+import { Link } from "react-router-dom"
 import "../../assets/css/main.css";
 
 function Navbar() {
   return (
-    <nav class="main-nav">
-      <a class="main-nav-logo" href="./index.html">
+    <nav className="main-nav">
+      <Link className="main-nav-logo" to="/">
         <img
-          class="main-nav-logo-image"
-          src="./img/argentBankLogo.png"
+          className="main-nav-logo-image"
+          src="../../img/argentBankLogo.png"
           alt="Argent Bank Logo"
         />
-        <h1 class="sr-only">Argent Bank</h1>
-      </a>
+        <h1 className="sr-only">Argent Bank</h1>
+      </Link>
+      
       <div>
-        <a class="main-nav-item" href="./sign-in.html">
-          <i class="fa fa-user-circle"></i>
+        <Link className="main-nav-item" to="/login">
+          <i className="fa fa-user-circle"></i>
           Sign In
-        </a>
+        </Link>
       </div>
     </nav>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar
