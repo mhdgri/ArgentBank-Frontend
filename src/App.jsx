4b0a4bda63c76824d59496ae50ccbd0ecfa45b7a
Mohamed Guerroui <mohamed.guerroui.mg@gmail.com>
2025-08-04 03:10:59 +0200

    configuration initiale des routes avec react-router-dom dans App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-
+import { Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
-
-
   return (
     <>
-
+      <Navbar />
+      <Routes>
+        <Route></Route>
+        <Route></Route>
+        <Route></Route>
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
