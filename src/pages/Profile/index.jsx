eec84aa4c25ab736da1ea7cadb7b9d81bfdecbfa
Mohamed Guerroui <mohamed.guerroui.mg@gmail.com>
2025-08-05 03:00:38 +0200

    Création de la page Profile

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,14 @@
+
+
+function Profile() {
+  return (
+<main class="main bg-dark">
+      <div class="header">
+        <h1>Welcome back<br />Tony Jarvis!</h1>
+        <button class="edit-button">Edit Name</button>
+      </div>
+    </main>
+  )
+}
+
+export default Profile
