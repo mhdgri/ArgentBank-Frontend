947c006d3b872026c0b98179b970d5ce51049433
Mohamed Guerroui <mohamed.guerroui.mg@gmail.com>
2025-08-05 03:43:17 +0200

    Ajout de la route Profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Navbar from "./components/Navbar/index";
+import Footer from "./components/Footer/index";
+import "./assets/css/main.css";
 
 function App() {
   return (
@@ -10,8 +12,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="Login" element={<Login />} />
-        <Route></Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
       <Footer />
     </>
