13e37fa59657d52fb8954d1a25211282f7206ffe
Mohamed Guerroui <mohamed.guerroui.mg@gmail.com>
2025-08-11 17:52:38 +0200

    Ajout du bouton 'View transactions' dans le composant Account ; git push

--- src/components/Account/index.jsx
+++ src/components/Account/index.jsx
@@ -9,6 +9,9 @@ function Account({ title, amount, description }) {
           <p className="account-amount">{amount}</p>
           <p className="account-amount-description">{description}</p>
         </div>
+        <div className="account-content-wrapper cta">
+          <button className="transaction-button">View transactions</button>
+        </div>
       </section>
     </div>
   );
