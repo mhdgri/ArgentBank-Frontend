f57325cc04d05f1ff0969d24e13e873669f21bd0
Mohamed Guerroui <mohamed.guerroui.mg@gmail.com>
2025-08-11 14:19:26 +0200

    Création du composant InputField avec PropTypes et passage des props depuis Login vers le composant enfant

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,25 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import InputField from "../../components/InputField";
+import { useNavigate } from "react-router-dom";
+import Profile from "../Profile";
 
 function Login() {
+  const navigate = useNavigate()
+  const [ email, setEmail ] = useState("")
+  const [ password, setPassword ] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    navigate('/Profile')
+  }
+  console.log(handleSubmit)
+
   return (
     <main className="main bg-dark">
       <section className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
-        <form>
-          <div className="input-wrapper">
-            <label htmlFor="username">Username</label>
-            <input type="text" id="username" />
-          </div>
-          <div className="input-wrapper">
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
-          </div>
+        <form onSubmit={handleSubmit} >
+          <InputField 
+            label="Username"
+            id="username"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value) }
+           />
+          <InputField
+            label="Password"
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value) }
+          />
           <div className="input-remember">
             <input type="checkbox" id="remember-me" />
             <label htmlFor="remember-me">Remember me</label>
           </div>
-          <button className="sign-in-button">Sign In</button>
+          <button
+            type="submit"
+            className="sign-in-button"
+          >
+            Sign In
+          </button>
         </form>
       </section>
     </main>
