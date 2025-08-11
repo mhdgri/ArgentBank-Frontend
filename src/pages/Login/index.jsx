d29d9fdc6e7c9eed5a4d2cdc5f59a7b705a92161
Mohamed Guerroui <mohamed.guerroui.mg@gmail.com>
2025-08-11 14:41:54 +0200

    Création du composant InputField avec PropTypes et déplacement des props vers le composant enfant

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,46 +2,48 @@ import React, { useState } from "react";
 import InputField from "../../components/InputField";
 import { useNavigate } from "react-router-dom";
 import Profile from "../Profile";
+import RememberMeCheckBox from "../../components/RememberMeCheckBox";
 
 function Login() {
-  const navigate = useNavigate()
-  const [ email, setEmail ] = useState("")
-  const [ password, setPassword ] = useState("")
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    navigate('/Profile')
-  }
-  console.log(handleSubmit)
+    e.preventDefault();
+    navigate("/Profile");
+  };
+  console.log(handleSubmit);
 
   return (
     <main className="main bg-dark">
       <section className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
-        <form onSubmit={handleSubmit} >
-          <InputField 
+        <form onSubmit={handleSubmit}>
+          <InputField
             label="Username"
             id="username"
             type="email"
             value={email}
-            onChange={(e) => setEmail(e.target.value) }
-           />
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <InputField
             label="Password"
             id="password"
             type="password"
             value={password}
-            onChange={(e) => setPassword(e.target.value) }
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <div className="input-remember">
-            <input type="checkbox" id="remember-me" />
-            <label htmlFor="remember-me">Remember me</label>
-          </div>
-          <button
-            type="submit"
-            className="sign-in-button"
-          >
+          <RememberMeCheckBox
+            label="Remember me"
+            id="remember-me"
+            type="checkbox"
+            checked={rememberMe}
+            onChange={() => setRememberMe(!rememberMe)}
+          />
+          <button type="submit" className="sign-in-button">
             Sign In
           </button>
         </form>
